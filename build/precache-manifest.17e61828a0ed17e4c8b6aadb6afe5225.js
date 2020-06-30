87a06b6ddabeee1bd15cb5a7d0a31647e3a6262c
henricasi <henriquecmandruzato@gmail.com>
2020-06-30 20:40:43 -0300

    Deploy 6th attempt

---
+++ build/precache-manifest.17e61828a0ed17e4c8b6aadb6afe5225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdf695f3a1d251a9ed0d49f7085cd21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc020138ceed33749cf7",
+    "url": "/static/css/main.6daf169f.chunk.css"
+  },
+  {
+    "revision": "547de9abd9de5ce7b615",
+    "url": "/static/js/2.622e0b9a.chunk.js"
+  },
+  {
+    "revision": "ab95c6ea939e836e2e627fa50fe64b65",
+    "url": "/static/js/2.622e0b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc020138ceed33749cf7",
+    "url": "/static/js/main.19aa49b4.chunk.js"
+  },
+  {
+    "revision": "b023dd1c8cbcb59954ef",
+    "url": "/static/js/runtime-main.3c281cb0.js"
+  }
+]);
